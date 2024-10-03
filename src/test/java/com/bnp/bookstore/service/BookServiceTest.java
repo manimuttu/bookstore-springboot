51e1bf914c94f7d509748bd2099afa4236c9e6d6
Ganesh Ambiger <ganesh.ambiger@wipro.com>
2024-10-03 16:31:57 +0530

    BookServiceTest added for BookService

---
+++ src/test/java/com/bnp/bookstore/service/BookServiceTest.java
@@ -0,0 +1,37 @@
+package com.bnp.bookstore.service;
+
+import com.bnp.bookstore.model.Book;
+import com.bnp.bookstore.repository.BookRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class BookServiceTest {
+
+    @Mock
+    private BookRepository bookRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    @Test
+    public void testAddBooks() {
+        Book book1 = mock(Book.class);
+        Book book2 = mock(Book.class);
+        List<Book> books = List.of(book1, book2);
+
+        when(bookRepository.saveAll(books)).thenReturn(books);
+        List<Book> addedBooks = bookService.addBooks(books);
+
+        assertEquals(2, addedBooks.size());
+        verify(bookRepository, times(1)).saveAll(books);
+    }
+}
