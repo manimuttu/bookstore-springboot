74e8ded2830206fa0540d0800bcd6ae120abe65e
Ganesh Ambiger <ganesh.ambiger@wipro.com>
2024-10-04 17:33:26 +0530

    Global exception changes - ServletException handle logic added

--- src/main/java/com/bnp/bookstore/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bnp/bookstore/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.bnp.bookstore.exception;
 
+import jakarta.servlet.ServletException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -23,4 +24,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("BookStore got unexpected error occurred: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(ServletException.class)
+    public ResponseEntity<String> handleServletException(ServletException ex) {
+        return ResponseEntity.status(400).body("Servlet exception occurred: " + ex.getMessage());
+    }
 }
